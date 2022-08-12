7e26cf6648acf8ab79bb3f1355063ef45c6fcc74
bumwoo <gld55602@gmail.com>
2022-08-13 03:09:18 +0900

    #10 - 테스트 데이터 data.sql 추가
    mockaroo로 생성 가능

---
+++ project-board/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at) values
+('hi', 'this is test', '#purple', 'beomu', 'beomu', '2021-05-06 11:51:24', '2021-05-23 08:34:54');
+insert into article_comment (article_id, content, created_at, modified_at, created_by, modified_by) values
+(1, 'this is comment test', '2021-03-02 22:40:04', '2021-04-27 15:38:09', 'beomu', 'beomu');
