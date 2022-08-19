bb07dc2a09487e43e4c236c7aa8e7167b8115299
bumwoo <gld55602@gmail.com>
2022-08-19 15:10:59 +0900

    메인 페이지 redirect -> forwarding 변경

---
+++ project-board/src/test/java/com/example/projectboard/controller/MainControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.projectboard.controller;
+
+import com.example.projectboard.config.SecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+}
